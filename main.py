17f3afd0abfef35748c209c97058c19f5b5597dd
Kashyap2way <128836459+Kashyap2way@users.noreply.github.com>
2023-10-24 16:30:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(21)
+
+try:
+	while True:
+    	servo.min()
+    	sleep(0.5)
+    	servo.mid()
+    	sleep(0.5)
+    	servo.max()
+    	sleep(0.5)
+except KeyboardInterrupt:
+	print("Program stopped")
