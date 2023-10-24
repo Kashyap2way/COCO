5e8d1930b68273a73e0d6fa6da96de72a3273b1c
Kashyap2way <128836459+Kashyap2way@users.noreply.github.com>
2023-10-24 17:15:16 +0530

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,15 +1,22 @@
 from gpiozero import Servo
 from time import sleep
 
-servo = Servo(21)
+# Define pins for the 12 servos
+servo_pins = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13]
+
+# Create a list of 12 servo objects
+servos = [Servo(pin) for pin in servo_pins]
+
+# Set all servos to 90 degrees
+for servo in servos:
+    servo.mid()
 
 try:
-	while True:
-		servo.min()
-		sleep(0.5)
-		servo.mid()
-		sleep(0.5)
-		servo.max()
-		sleep(0.5)
+    while True:
+        # Your code can go here, or you can leave the loop empty to keep the servos at 90 degrees.
+        sleep(1)  # Sleep to reduce CPU usage
+
 except KeyboardInterrupt:
-	print("Program stopped")
+    # Clean up when the program is interrupted
+    for servo in servos:
+        servo.detach()
