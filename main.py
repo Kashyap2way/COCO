0a19ae3eaa22fe2b4fc2963ccc481c4f5a9a56d1
Kashyap2way <128836459+Kashyap2way@users.noreply.github.com>
2023-10-24 19:23:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-import Stand_angle_adjust
+from gpiozero import Servo
+from time import sleep
 
-stand_adjust(124,135,200)
+# Define pins for the 12 servos
+servo_pins = [23, 25, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13]
+
+# Create a list of 12 servo objects
+servos = [Servo(pin) for pin in servo_pins]
+
+try:
+    while True:
+        for servo in servos:
+            servo.min()  # Set each servo to 45 degrees
+            sleep(1)  # Wait for 1 second
+            servo.mid()  # Set each servo to 90 degrees
+            sleep(1)  # Wait for 1 second
+
+except KeyboardInterrupt:
+    # Clean up when the program is interrupted
+    for servo in servos:
+        servo.detach()
+
