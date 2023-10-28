7de99bc2264847006f4ff26934aaf8d1407113f5
Kashyap2way <128836459+Kashyap2way@users.noreply.github.com>
2023-10-28 19:14:11 +0530

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -1,4 +1,6 @@
 from math import *
+from gpiozero import Servo
+from time import sleep
 
 def stand(hypo):
     hypo = hypo/1000
@@ -15,4 +17,23 @@ def stand(hypo):
 
     return theta_b, theta_c, pwm_b, pwm_c
 
+# Define pins for the 12 servos
+servo_pins = [23, 25, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13]
 
+# Create servo objects for each pin
+servos = [Servo(pin) for pin in servo_pins]
+
+# Define the angles
+angle_90 = 0
+angle_45 = -0.5  # Adjust this value if needed
+
+try:
+    while True:
+        # Set all servos to 90 degrees
+        for servo in servos:
+            servo.value = angle_90
+
+except KeyboardInterrupt:
+    # Clean up when the program is terminated
+    for servo in servos:
+        servo.close()
