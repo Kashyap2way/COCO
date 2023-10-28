b68155b28e5837ac3c6759352eab4a2fd89a6488
Kashyap2way <128836459+Kashyap2way@users.noreply.github.com>
2023-10-28 19:21:58 +0530

    Update servo2.py

--- servo2.py
+++ servo2.py
@@ -18,9 +18,9 @@ def stand(hypo):
     return theta_b, theta_c, pwm_b, pwm_c
 
 # Define pins for the 12 servos
-servo_pins = [23, 25, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13]
+#servo_pins = [23, 25, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13
 
-FRS_pin = [9, 4, 27, 11, 6, 25,
+FRS_pin = 9
 FLS_pin = 4
 BRS_pin = 27
 BLS_pin = 11
@@ -41,17 +41,17 @@ GPIO.setup(BRE_pin, GPIO.OUT)
 GPIO.setup(BLE_pin, GPIO.OUT)
 
 # Create servo objects for each pin
-servos = [Servo(pin) for pin in servo_pins]
+#servos = [Servo(pin) for pin in servo_pins]
 
 # Define the angles
-angle_90 = 0
-angle_45 = -0.5  # Adjust this value if needed
+#angle_90 = 0
+#angle_45 = -0.5  # Adjust this value if needed
 
 try:
     while True:
         # Set all servos to 90 degrees
-        for servo in servos:
-            servo.value = angle_90
+        
+        FRS_pin.value, FRE_pin.value = 
 
 except KeyboardInterrupt:
     # Clean up when the program is terminated
