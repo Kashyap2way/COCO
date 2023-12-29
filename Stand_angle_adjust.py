664287abbb89fdff66bff80151ad9407e1b92068
RugvedTamhan <92918609+RugvedTamhan@users.noreply.github.com>
2023-12-30 01:27:34 +0530

    Update Stand_angle_adjust.py

--- Stand_angle_adjust.py
+++ Stand_angle_adjust.py
@@ -1,17 +1,25 @@
 from math import *
 
 def stand(link1, link2, hypo):
-    cos_theta_b = (link1**2 + hypo**2 - link2**2)/(2*link1*hypo)
 
+    cos_theta_c = (link1**2 + hypo**2 - link2**2)/(2*link1*hypo)
+    theta_c = int(degrees(acos(cos_theta_c)))
+
+    cos_theta_b = (hypo**2 + link2**2 - link1**2)/(2*hypo*link2)
     theta_b = int(degrees(acos(cos_theta_b)))
-    theta_c = (180-(theta_b*2))
-    
-    theta_b += 52
-    theta_c -= 14
 
-    pwm_b = (theta_b - 90)/90
-    pwm_c = (theta_c - 90)/90
+    theta_a = (180-(theta_c + theta_b))
 
-    return theta_b, theta_c, pwm_b, pwm_c
+    F_angle1 = 40 #Default angle at some shoulder motor angle (90 deg)
+    F_angle2 = 40 #Default angle at some elbow motor angle (90 deg)
 
-print(stand(0.122,0.130,0.2))
+    angle_s = 90 + (theta_c - F_angle1)
+    angle_e = 90 + (theta_a - F_angle2)
+
+    # angle_s and angle_e are the actual servo motor angle. Convert this to suitable pwm as per the motor:
+
+
+    return theta_c, theta_a, angle_s, angle_e
+
+
+print(stand(0.122,0.130,0.17))
